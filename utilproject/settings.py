9177cd36856dd68fdb0c12ca77f292a9ffeee210
pimpim13 <alain.zypinoglou@gmail.com>
2022-03-11 17:35:28 +0100

    Ajout image  ind_dep

--- utilproject/settings.py
+++ utilproject/settings.py
@@ -25,10 +25,11 @@ environ.Env.read_env(env_file=str(BASE_DIR / "utilproject" / ".env"))
 SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env.bool("DEBUG", False)
-
-ALLOWED_HOSTS = env.list('ALLOWED_HOSTS')
-
+# DEBUG = env.bool("DEBUG", False)
+DEBUG = True
+# ALLOWED_HOSTS = env.list('ALLOWED_HOSTS')
+ALLOWED_HOSTS = ['127.0.0.1']
+#
 # Application definition
 
 INSTALLED_APPS = [
