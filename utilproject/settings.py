29ebf62849f25d669b72044e27780c5851e3aa14
pimpim13 <alain.zypinoglou@gmail.com>
2022-05-02 11:35:17 +0000

    save settings

--- utilproject/settings.py
+++ utilproject/settings.py
@@ -28,7 +28,9 @@ SECRET_KEY = env("SECRET_KEY")
 # DEBUG = env.bool("DEBUG", False)
 DEBUG = True
 # ALLOWED_HOSTS = env.list('ALLOWED_HOSTS')
-ALLOWED_HOSTS = ['127.0.0.1']
+
+ALLOWED_HOSTS = ['cfdtrte.pythonanywhere.com',
+                '127.0.0.1',]
 #
 # Application definition
 
