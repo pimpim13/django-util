11b9d13c9088aeeaf1b928de47c16e4cccb8e678
pimpim13 <alain.zypinoglou@gmail.com>
2025-05-05 15:46:45 +0200

    mise a jour pim

--- pim/api.py
+++ pim/api.py
@@ -2,23 +2,16 @@ class Indemnisation:
 
     FRANCHISE_MIN = 30
     FRANCHISE_MAX = 60
-    TX_INDEMNISATION_TPS = 13.64
+    TX_INDEMNISATION_TPS = 14.78
     TX_INDEMNISATION_KM = 0.315
 
     def __init__(self, dico):
         self.aller_actuel_mn = int(dico.get("aller_actuel_mn", "0"))
-        self.aller_actuel_km = int(dico.get("aller_actuel_km", 0))
-        # self.retour_actuel_mn = int(dico.get("aller_actuel_mn", 0))
-        # self.retour_actuel_km = int(dico.get("aller_actuel_km", 0))
-
         self.aller_futur_mn = int(dico.get("aller_futur_mn", 0))
-        self.aller_futur_km = int(dico.get("aller_futur_km", 0))
-        # self.retour_futur_mn = int(dico.get("aller_futur_mn", 0))
-        # self.retour_futur_km = int(dico.get("aller_futur_km", 0))
 
         self.duree_tx_futur = float(dico.get("duree_tx_future", 194))
         self.eligible_tt = dico.get("eligible_tt", False)
-        # self.teletravail_futur = float(dico.get("teletravail_futur", 0))
+
         self.nb_jours_tt = int(dico.get("nb_jours_tt", 108))
 
     def allongement_tps(self, actuel=0, futur=0):
@@ -34,50 +27,37 @@ class Indemnisation:
     def compute(self):
 
         allongement_tps_aller = self.allongement_tps(self.aller_actuel_mn, self.aller_futur_mn)
-        # allongement_tps_futur = self.allongement_tps(self.retour_actuel_mn, self.retour_futur_mn)
-        # tps = allongement_tps_aller + allongement_tps_futur
         tps = allongement_tps_aller * 2
 
-        allongement_km_aller = self.allongement_km(self.aller_actuel_km, self.aller_futur_km)
-        # allongement_km_retour = self.allongement_km(self.retour_actuel_km, self.retour_futur_km)
-        # km = allongement_km_aller + allongement_km_retour
-        km = allongement_km_aller * 2
+        """allongement_km_aller = self.allongement_km(self.aller_actuel_km, self.aller_futur_km)"""
+        """km = allongement_km_aller * 2 * 0
 
-        total_km = round(km * Indemnisation.TX_INDEMNISATION_KM * self.duree_tx_futur * 3, 2)
+        total_km = round(km * Indemnisation.TX_INDEMNISATION_KM * self.duree_tx_futur * 3, 2)"""
         total_tps = round(tps * Indemnisation.TX_INDEMNISATION_TPS/60 * self.duree_tx_futur * 3, 2)
 
         if self.eligible_tt:
 
-            self.reste_jour_tt = self.duree_tx_futur - int(self.nb_jours_tt * 0.75)
+            self.reste_jour_tt_75 = self.duree_tx_futur - int(self.nb_jours_tt * 0.75)
+            self.reste_jour_tt_50 = self.duree_tx_futur - int(self.nb_jours_tt * 0.5)
+            total_tps_tt_75 = round(tps * Indemnisation.TX_INDEMNISATION_TPS/60 * self.reste_jour_tt_75 * 3, 2)
+            total_tps_tt_50 = round(tps * Indemnisation.TX_INDEMNISATION_TPS/60 * self.reste_jour_tt_50 * 3, 2)
 
-            # total_tps_tt = round(tps * Indemnisation.TX_INDEMNISATION_TPS/60 * (self.duree_tx_futur - 84) * 3, 2)
-            total_tps_tt = round(tps * Indemnisation.TX_INDEMNISATION_TPS/60 * self.reste_jour_tt * 3, 2)
-            # total_km_tt= round(km * Indemnisation.TX_INDEMNISATION_KM * (self.duree_tx_futur - 84) * 3, 2)
-            total_km_tt= round(km * Indemnisation.TX_INDEMNISATION_KM * self.reste_jour_tt * 3, 2)
         else:
 
-            total_tps_tt= 0
-            total_km_tt= 0
+            total_tps_tt_50= 0
+            total_tps_tt_75= 0
 
-        print(type(self.eligible_tt), total_tps_tt, total_km_tt)
 
-        # total_km_tt = round(total_km * self.coeff_tt(self.duree_tx_futur), 2)
-        # total_km_tt = round(total_km * self.coeff_tt(self.duree_tx_futur), 2)
-
-        return {"tps-travail": {"valeur": self.duree_tx_futur,
+        return {"tps_travail": {"valeur": self.duree_tx_futur,
                                 "label": "Durée de Travail", "unite": "Jours /an", "url": "helptps"},
                 "ecart_tps": {"valeur": tps,
-                              "label": "Ecart en temps retenu", "unite": "mn /Jour", "url": "tpsret"},
+                              "label": "Ecart retenu", "unite": "mn /Jour", "url": "tpsret"},
                 "indem_tps": {"valeur": total_tps,
-                              "label": "Indemnisation en temps", "unite": "€", "url": "indtps"},
-                "allong_km": {"valeur": km,
-                              "label": "Ecart en km retenu", "unite": "km", "url": "helpkmret"},
-                "indem_km": {"valeur": total_km,
-                             "label": "Indemnisation km", "unite": "€", "url": "helpindkm"},
-                "indem_tps_tt": {"valeur": total_tps_tt,
-                                 "label": "Indemnisation en temps avec TT", "unite": "€", "url": "helptt"},
-                "indem_km_tt": {"valeur": total_km_tt,
-                                "label": "Indemnisation km avec TT", "unite": "€", "url": "helptt"},
+                              "label": "Indemnisation", "unite": "€", "url": "indtps"},
+                "indem_tps_tt_75": {"valeur": total_tps_tt_75,
+                                 "label": "Indemnisation direction avec TT (-75%)", "unite": "€", "url": "helptt"},
+                "indem_tps_tt_50": {"valeur": total_tps_tt_50,
+                                "label": "Indemnisation avec TT (-50%)", "unite": "€", "url": "helptt"},
                 }
 
     def coeff_tt(self, dt):
--- pim/forms.py
+++ pim/forms.py
@@ -7,7 +7,7 @@ class DataForm(forms.Form):
 
     CHOICES = [(194, '5J / sem'),
                (184, '4J / sem'),
-               (190, 'CFJ'),
+               (190, 'CFJ 190'),
                (197, 'CFJ 197'),
                (203, 'CFJ 203'),
                (209, 'CFJ 209'),
@@ -23,21 +23,29 @@ class DataForm(forms.Form):
                    (False, 'Non'),
                ]
 
-    NB_JOURS_TT = [(108, "108 jours/an"),
-                   (20, "20 jours/an"),
-                   (10, "10 jours/an")]
+    NB_JOURS_TT = [(108, "108 jours"),
+                   (20, "20 jours"),
+                   (10, "10 jours")]
 
 
     CHOIXTC = [(True, 'Oui'),
                (False, 'Non'),
                ]
 
-    aller_actuel_mn = forms.IntegerField(label='Durée trajet actuel', max_value=240, min_value=0)
-    aller_futur_mn = forms.IntegerField(label='Durée trajet futur', max_value=240, min_value=0)
-    aller_actuel_km = forms.IntegerField(label='Distance km actuel', max_value=200, min_value=0)
-    aller_futur_km = forms.IntegerField(label='Distance km futur', max_value=200, min_value=0)
-    duree_tx_future = forms.ChoiceField(choices=CHOICES, widget=forms.RadioSelect)
-    eligible_tt = forms.BooleanField(label="Eligible au Télétravail ?", widget=forms.CheckboxInput, required=False)
-    nb_jours_tt = forms.ChoiceField(choices=NB_JOURS_TT, widget=forms.RadioSelect)
+    CHOICES_MEMO = [(True, ""), (False, "")]
+
+    aller_actuel_mn = forms.IntegerField(label='Durée trajet "aller" actuel', max_value=240, min_value=0)
+    aller_futur_mn = forms.IntegerField(label='Durée trajet "aller" futur', max_value=240, min_value=0)
+    # aller_actuel_km = forms.IntegerField(label='Distance km actuel', max_value=200, min_value=0)
+    # aller_futur_km = forms.IntegerField(label='Distance km futur', max_value=200, min_value=0)
+    duree_tx_future = forms.ChoiceField(label="Votre aménagement temps de travail", choices=CHOICES)
+    # duree_tx_future = forms.ChoiceField(choices=CHOICES, widget=forms.RadioSelect)
+    eligible_tt = forms.BooleanField(label="Eligible au Télétravail ?", widget=forms.CheckboxInput, required=False, initial=True)
+    nb_jours_tt = forms.ChoiceField(label="Plafond annuel théorique de télétravail", choices=NB_JOURS_TT, widget=forms.RadioSelect)
+    # memo = forms.BooleanField(label="Se souvenir de moi", widget=forms.CheckboxInput,
+    #                          initial=True, required=False)
+    #
+    #
+    # print('valeur de memo : ', memo)
 
 
