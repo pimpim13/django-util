e68a6a92d869802cec7e57324248b74b0f69e891
pimpim13 <alain.zypinoglou@gmail.com>
2025-09-19 16:05:38 +0000

    application des modifications snb/ urls forms template/transpo

--- snb/forms.py
+++ snb/forms.py
@@ -1,13 +1,15 @@
+#snb/forms.py
 from django import forms
-from snb.models import Snb_ref, Coeff, Echelon
+from snb.models import Echelon, Coeff_New, Snb_ref_New
+from datetime import date
 
 
 class SnbUpdateForm(forms.ModelForm):
 
     class Meta:
-        model = Snb_ref
-        fields = ['annee', 'snb']
-        labels = {'annee': 'Année', 'snb': 'SNB'}
+        model = Snb_ref_New
+        fields = ['annee', 'snb', 'date_application']
+        labels = {'annee': 'Année', 'snb': 'SNB', 'date_application': "Date d'application"}
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -17,9 +19,10 @@ class SnbUpdateForm(forms.ModelForm):
 class SnbCreateForm(forms.ModelForm):
 
     class Meta:
-        model = Snb_ref
-        fields = ['annee', 'snb']
-        labels = {'annee': 'Année', 'snb': 'SNB'}
+        # model = Snb_ref
+        model = Snb_ref_New
+        fields = ['annee', 'snb', 'date_application']
+        labels = {'annee': 'Année', 'snb': 'SNB', 'date_application': "Date d'application"}
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -28,26 +31,52 @@ class SnbCreateForm(forms.ModelForm):
 
 class CalculSalaireForm(forms.Form):
 
-    YEAR_CHOICES = [(a.annee, a.annee) for a in Snb_ref.objects.all()]
-#    YEAR_CHOICES = [(2022, 2022),]
-    NR_CHOICE = [(a.valeur, a.NR) for a in Coeff.objects.all()]
+    ARTT32CO = float(32/35)
+
+    # YEAR_CHOICES = [(a.annee, a.annee) for a in Snb_ref_New.objects.all()]
+    YEAR_CHOICES = [(a.date_application, a.date_application) for a in Snb_ref_New.objects.all()]
+    NR_CHOICE = [(a.NR, a.NR) for a in Coeff_New.objects.filter(date_application=date(1900, 1, 1))]
     ECHELON_CHOICE = [(a.coeff, a.echelon) for a in Echelon.objects.filter(echelon__gt=3)]
     MAJ_RES_CHOICE = [(1.24, '24%'), (1.245, '24,5%'), (1.25, '25%')]
-    TPS_TRAV_CHOICE = [(1, '35h'), (0.971, '32h Coll.'), (0.943, '32h Indiv.'), (0.707, "24h")]
+    TPS_TRAV_CHOICE = [(1, '35h'), (0.9714285714, '32h Coll.'), (0.9428571429, '32h Indiv.'), (0.707, "24h")]
     CHOICES_MEMO = [(True, ""), (False, "")]
 
-    annee = forms.ChoiceField(label='Année', choices=YEAR_CHOICES, initial=YEAR_CHOICES[0][0])
+    date_application = forms.ChoiceField(label='Année', choices=YEAR_CHOICES, initial=YEAR_CHOICES[0][0])
     Nr = forms.ChoiceField(label='Nr actuel', choices=NR_CHOICE)
     echelon = forms.ChoiceField(label="Echelon actuel", choices=ECHELON_CHOICE)
     maj_res = forms.ChoiceField(label='Maj rés', choices=MAJ_RES_CHOICE)
     tps_trav = forms.ChoiceField(label='ATT', choices=TPS_TRAV_CHOICE)
-    Nr_futur = forms.ChoiceField(label='Nr futur', choices=NR_CHOICE)
-    echelon_futur = forms.ChoiceField(label="Ech futur", choices=ECHELON_CHOICE)
+    # Nr_futur = forms.ChoiceField(label='Nr futur', choices=NR_CHOICE)
+    # echelon_futur = forms.ChoiceField(label="Ech futur", choices=ECHELON_CHOICE)
     memo = forms.ChoiceField(label="Se souvenir de moi", widget=forms.CheckboxInput,
                              initial=True, choices=CHOICES_MEMO, required=False)
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.fields['annee'].disabled = True
+        # self.fields['annee'].disabled = False
 
+
+class EvolSnbForm(forms.Form):
+
+    NR_CHOICE = [(a.NR, a.NR) for a in Coeff_New.objects.filter(date_application=date(1900, 1, 1))]
+    ECHELON_CHOICE = [(a.coeff, a.echelon) for a in Echelon.objects.filter(echelon__gt=3)]
+    # MAJ_RES_CHOICE = [(1.24, '24%'), (1.245, '24,5%'), (1.25, '25%')]
+    MAJ_RES_CHOICE = [(1.25, '25%'), (1.245, '24,5%'), (1.24, '24%')]
+    TPS_TRAV_CHOICE = [(1, '35h'), (0.9714285714, '32h Coll.'), (0.9428571429, '32h Indiv.'), (0.707, "24h")]
+
+    Nr = forms.ChoiceField(label='Nr ', choices=NR_CHOICE)
+    echelon = forms.ChoiceField(label="Echelon actuel", choices=ECHELON_CHOICE)
+    maj_res = forms.ChoiceField(label='Maj rés', choices=MAJ_RES_CHOICE)
+    tps_trav = forms.ChoiceField(label='ATT', choices=TPS_TRAV_CHOICE)
+    toto = forms.BooleanField(widget=forms.HiddenInput, initial=True, required=False)  # champs caché pour selection de formulaire
+
+class TranspositionForm(EvolSnbForm):
+
+    # TPS_TRAV_CHOICE_TRANSPO = [(1, '35h ou CFJ'), (0.9714285714, '32h Coll.'), (0.9428571429, '32h Indiv.'),]
+    TPS_TRAV_CHOICE_TRANSPO = [(1, 'Indifferent'), (0.9714285714, '32h Coll.'), (0.9428571429, '32h Indiv.'),]
+    tps_trav = forms.ChoiceField(label='ATT', choices=TPS_TRAV_CHOICE_TRANSPO,)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['tps_trav'].disabled = True
 
--- snb/urls.py
+++ snb/urls.py
@@ -1,14 +1,24 @@
+#snb/urls.py
 from django.contrib import admin
 from django.urls import path, include, re_path
 from . import views
-from snb.views import SnbListView, SnbUpdate
+from snb.views import SnbListView, SnbUpdate, compute, test
 
 urlpatterns = [
     re_path(r'^$', views.snb, name='snb'),
+    # re_path(r'^#result/$', views.snb, name='snb_result'),
     # path('update/snb/', views.snb_list, name='snb_list'),
     path('update/snb/', SnbListView.as_view(), name='snb_list'),
     # path('update/snb/<str:item>/', views.snb_update_item, name='snb_update_item'),
     path('update/snb/<int:pk>/', SnbUpdate.as_view(), name='snb_update_item'),
     path('delete/snb/<str:item>/', views.snb_delete_item, name='snb_delete_item'),
     path('new/snb/', views.snb_new, name='snb_new_item'),
+
+    path('evolution/', views.snb_evol, name='snb_evol'),
+    path('evolution/compute/', views.compute, name='compute'),
+
+    path('transposition/', views.snb_transpose, name='snb_transposition'),
+    path('transposition/compute/', views.transpose_compute, name='transpose_compute'),
+
+    path('test/', views.test, name='test'),
 ]
